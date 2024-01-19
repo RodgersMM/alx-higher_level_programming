143e94e122807ea4b47d0dc82bd6a2751bb59067
RodgersMM <rodgers>
2024-01-18 16:07:32 -0800

    0x02. Python - import & modules

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function args & returns
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """My subtraction function args & returns
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
+
+
+def mul(a, b):
+    """My multiplication function args & returns
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
+
+
+def div(a, b):
+    """My division function args & returns
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+	 The return value. a / b
+    """
+    return int(a / b)
